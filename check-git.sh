1bda3a64ecb8748ab4e65a4eccfd76f06bf93a84
Fabio Rosada <r.fabio95@gmail.com>
2024-03-29 11:30:14 +0100

    added control in preversion script to check if we are on 'main' branch

--- check-git.sh
+++ check-git.sh
@@ -3,6 +3,15 @@ if [ -z "$1" ]; then
   echo "Please provide a commit message"
   exit 1
 fi
+
+# Check if on "main" branch
+if [[ $(git rev-parse --abbrev-ref HEAD) != "main" ]]; then
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  echo -e "\033[31mYou can release a new version only from the 'main' branch."
+  echo -e "\033[0mCurrent branch: $current_branch"
+  exit 1
+fi
+
 if [[ `git status --porcelain` ]]; then
   echo "There are changes"
   git add .
